955f7e598a22c97c4af00a14f8fd25cd7343a6d9
SOW_ZOM <o26032002z@gmail.com>
2020-05-03 19:25:59 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_OperatorsOverloading_Indexers
+{
+    public class PersonComplexNumber
+    {
+       public int real;
+       public int imaginary;
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
